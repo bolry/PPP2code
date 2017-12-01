37e195a5a61c934587dfe418110d6981a65dc41a
Bo Rydberg <bolry@hotmail.com>
2017-12-01 23:05:47 +0200

    Add step 3 of chap 3 drill

--- chap3drill/letter.cpp
+++ chap3drill/letter.cpp
@@ -4,6 +4,18 @@ int main() {
 	cout << "Enter the name of the person you want to write to: ";
 	string first_name; // first_name is a variable of type string
 	cin >> first_name; // read characters into first_name
-	cout << "Dear " << first_name << ",\n\n"
-			"    How are you?  I am fine.  I miss you.\n";
+	cout << "Enter the name of another friend: ";
+	string friend_name;
+	cin >> friend_name;
+	cout << "Is " << friend_name
+			<< " male, (enter 'm') or female (enter 'f'): ";
+	char friend_sex = 0;
+	cin >> friend_sex;
+	cout << "Dear " << first_name
+			<< ",\n\n    How are you?  I am fine.  I miss you.\n"
+			<< "Have you seen " << friend_name << " lately?  ";
+	if (friend_sex == 'f')
+		cout << "If you see " << friend_name << " please ask her to call me.\n";
+	if (friend_sex == 'm')
+		cout << "If you see " << friend_name << " please ask him to call me.\n";
 }

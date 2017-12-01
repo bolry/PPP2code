57a7284f8a352cc33ef77f3b4ea6a97ccb160f85
Bo Rydberg <bolry@hotmail.com>
2017-12-01 22:46:46 +0200

    Add step 2 of chap 3 drill

--- chap3drill/letter.cpp
+++ chap3drill/letter.cpp
@@ -4,5 +4,6 @@ int main() {
 	cout << "Enter the name of the person you want to write to: ";
 	string first_name; // first_name is a variable of type string
 	cin >> first_name; // read characters into first_name
-	cout << "Dear " << first_name << ",\n";
+	cout << "Dear " << first_name << ",\n\n"
+			"    How are you?  I am fine.  I miss you.\n";
 }

ce6190fb94ebded82a445257b1f6db85d6468754
Bo Rydberg <bolry@hotmail.com>
2017-12-03 23:08:20 +0100

    Step 5 (pre) of chapter 5 drill

--- chap5drill/error_practice.cpp
+++ chap5drill/error_practice.cpp
@@ -2,8 +2,10 @@
 
 int main()
 try {
-	const string success = "Success!";
-	cout << success << '\n';
+	string res = 7;
+	vector<int> v(10);
+	v[5] = res;
+	cout << "Success!\n";
 	keep_window_open();
 	return 0;
 }

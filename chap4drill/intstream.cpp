beb6e8bf8d06568870e7ec944a93979e6f5eee2a
Bo Rydberg <bolry@hotmail.com>
2017-12-03 16:55:51 +0200

    Step 2 of chapter 4 drill

--- chap4drill/intstream.cpp
+++ chap4drill/intstream.cpp
@@ -1,10 +1,21 @@
 #include "std_lib_facilities.h"
 
 int main() {
+	cout << "write pairs of white-separated integer, terminate with '|': ";
 	int val1;
 	int val2;
 	while (cin >> val1 >> val2) {
-		cout << val1 << ", " << val2 << '\n';
+		cout << "the smaller value is: ";
+		if (val1 < val2)
+			cout << val1;
+		else
+			cout << val2;
+		cout << " the larger value is: ";
+		if (val1 > val2)
+			cout << val1;
+		else
+			cout << val2;
+		cout << '\n';
 	}
 	return 0;
 }

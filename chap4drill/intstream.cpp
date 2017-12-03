c051dadb06a9219fccc09b31ecb415ee11c0aeb7
Bo Rydberg <bolry@hotmail.com>
2017-12-03 16:59:56 +0200

    Step 3 of chapter 4 drill

--- chap4drill/intstream.cpp
+++ chap4drill/intstream.cpp
@@ -5,16 +5,20 @@ int main() {
 	int val1;
 	int val2;
 	while (cin >> val1 >> val2) {
-		cout << "the smaller value is: ";
-		if (val1 < val2)
-			cout << val1;
-		else
-			cout << val2;
-		cout << " the larger value is: ";
-		if (val1 > val2)
-			cout << val1;
-		else
-			cout << val2;
+		if (val1 == val2)
+			cout << "the numbers are equal";
+		else {
+			cout << "the smaller value is: ";
+			if (val1 < val2)
+				cout << val1;
+			else
+				cout << val2;
+			cout << " the larger value is: ";
+			if (val1 > val2)
+				cout << val1;
+			else
+				cout << val2;
+		}
 		cout << '\n';
 	}
 	return 0;
